43084307022b46cafff758473dfef7acf4f490ff
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-01-12 20:25:44 -0300

    feat: adiciona novas views e monta a tela

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -6,7 +6,17 @@ export default function Index() {
       style={styles.container}
     >
       <Image source={require('../assets/images/pomodoro.png')}/>
-      <View style={styles.actions} />
+      <View style={styles.actions}>
+        <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Projeto fictício e sem fins comerciais.</Text>
+        <Text style={styles.footerText}>Desenvolvido por Daniel Zanotelli.</Text>
+        
+      </View>
     </View>
   );
 }
@@ -21,11 +31,44 @@ const styles = StyleSheet.create({
   },
   
   actions: {
+    gap: 32,
     padding: 24,
     backgroundColor: "#14448080",
     borderRadius: 32,
     width: '80%',
     borderWidth: 2,
     borderColor: "#144480",
-  }
+    alignItems: "center",
+    alignContent: "center",
+  },
+  
+  timer: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontSize: 54,
+  },
+  
+  button: {
+    backgroundColor: "#B872FF",
+    padding: 8,
+    width: "100%",
+    alignItems: "center",
+    borderRadius: 32,
+  },
+  
+  buttonText: {
+    fontWeight: "bold",
+    color: "#021123",
+    fontSize: 18,
+  },
+  
+  footer: {
+    width: "80%",
+  },
+  
+  footerText: {
+    color: "#98A0A8",
+    textAlign: "center",
+    fontSize: 12.5
+  },
 })
