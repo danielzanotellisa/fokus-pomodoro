b6135bac90386f0273282a2f17d543770ba17dc7
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-01-15 03:06:41 -0300

    feat: cria componente `Timer`

---
+++ components/Timer/index.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text } from "react-native"
+
+export const Timer = ({totalSeconds}) => {
+    const date = new Date(totalSeconds * 1000)
+    const options = {minute: '2-digit', second: '2-digit'}
+    return (
+        <Text style={styles.timer}>
+            {date.toLocaleTimeString('pt-BR',options )}
+        </Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    timer: {
+        color: "#FFFFFF",
+        fontWeight: "bold",
+        fontSize: 54,
+      },
+})
