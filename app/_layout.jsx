97c8bbc503d7bc337f0f259f6bda178178503158
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-01-18 14:40:14 -0300

    refactor: adiciona opção para esconder o header da página `pomodoro`

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router"
 
 export default function StackLayout() {
-    return <Stack/>
+    return (
+        <Stack> 
+            <Stack.Screen name="pomodoro" options={{headerShown: false}}/>
+        </Stack>
+    )
 }
