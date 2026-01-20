8777616f91236133708a243d863168137303b656
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-01-19 21:28:44 -0300

    feat: adiciona opção `outlier`

--- components/FokusButton/index.jsx
+++ components/FokusButton/index.jsx
@@ -1,10 +1,10 @@
 import { Pressable, StyleSheet, Text } from "react-native";
 
-export const FokusButton = ({onPress, display, icon}) => {
+export const FokusButton = ({onPress, display, icon, outlier}) => {
     return (
-        <Pressable style={styles.button} onPress={onPress}>
+        <Pressable style={outlier ? styles.buttonOutlier : styles.button} onPress={onPress}>
 					{icon}
-          <Text style={styles.buttonText}>{display}</Text>
+          <Text style={outlier ? styles.buttonTextOutlier : styles.buttonText}>{display}</Text>
         </Pressable>
     )
 }
@@ -25,6 +25,26 @@ const styles = StyleSheet.create({
     buttonText: {
       fontWeight: "bold",
       color: "#021123",
+      fontSize: 18,
+    },
+    
+    buttonOutlier: {
+      flexDirection: "row",
+			alignContent: "center",
+			justifyContent: "center",
+      backgroundColor: "transparent",
+      borderColor: "#B872FF",
+      borderWidth: 2,
+      padding: 8,
+      width: "100%",
+      alignItems: "center",
+      borderRadius: 32,
+			gap: 8,
+    },
+    
+    buttonTextOutlier: {
+      fontWeight: "bold",
+      color: "#B872FF",
       fontSize: 18,
     },
 })
