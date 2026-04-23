6f41dcf8bd7a743152ce8170784730926ab4779c
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-04-23 19:37:34 -0300

    feat: cria estilização condicional para tarefas

--- components/TaskItem/index.jsx
+++ components/TaskItem/index.jsx
@@ -1,31 +1,40 @@
-import { Pressable, StyleSheet, Text, View } from "react-native"
-import { Edit, IconCheck } from "../Icons"
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Edit, IconCheck } from "../Icons";
 
-const TaskItem = ({completed, text, onToggleComplete, onPressEdit}) => {
-    return (
-        <View style={styles.container}>
-            <Pressable onPress={onToggleComplete}>
-                <IconCheck checked={completed}/>
-            </Pressable>
-            
-            <Text>{text}</Text>
-            <Pressable onPress={onPressEdit}>
-                <Edit/>
-            </Pressable>
-        </View>
-    )
-}
+const TaskItem = ({ completed, text, onToggleComplete, onPressEdit }) => {
+  const cardStyles = [styles.container];
+  if (completed) {
+    cardStyles.push(styles.cardCompleted);
+  }
+
+  return (
+    <View style={cardStyles}>
+      <Pressable onPress={onToggleComplete}>
+        <IconCheck checked={completed} />
+      </Pressable>
+
+      <Text>{text}</Text>
+      <Pressable onPress={onPressEdit}>
+        <Edit />
+      </Pressable>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        padding: 8,
-        borderRadius: 8,
-        backgroundColor: "#98A0A8",
-        width: "100%",
-        justifyContent: "space-between",
-        alignItems: "center"
-    }
-})
+  container: {
+    flexDirection: "row",
+    padding: 8,
+    borderRadius: 8,
+    backgroundColor: "#98A0A8",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
 
-export default TaskItem
+  cardCompleted: {
+    backgroundColor: "#0F725C",
+  },
+});
+
+export default TaskItem;
